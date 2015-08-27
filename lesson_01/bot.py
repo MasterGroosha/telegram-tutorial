b8e3467fb9cd66e85ece6ba87d5164e12b61afe5
Kondra007 <kondra007@yandex.ru>
2015-08-27 23:43:29 +0300

    Первый урок. Простой Echo-бот

---
+++ lesson_01/bot.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import time
+
+import telebot
+
+from lesson_01 import config
+
+
+def listener(messages):
+    for m in messages:
+        if m.content_type == 'text':
+            bot.send_message(m.chat.id, m.text)
+
+
+if __name__ == '__main__':
+    bot = telebot.TeleBot(config.token)
+    bot.set_update_listener(listener)
+    bot.polling(none_stop=True)
+    while True:
+        time.sleep(200)
