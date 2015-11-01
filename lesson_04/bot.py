cea340754e2cfd13e38fe156355d20ba62a5263b
Kondra007 <kondra007@yandex.ru>
2015-11-01 15:08:14 +0300

    Небольшие исправления в уроке 4. Обновлён Readme.

--- lesson_04/bot.py
+++ lesson_04/bot.py
@@ -4,9 +4,9 @@
 import telebot
 import cherrypy
 
-from lesson_01 import config
+from lesson_04 import config
 
-WEBHOOK_HOST = '<ip/host where the bot is running>'
+WEBHOOK_HOST = 'ip/host where the bot is running'
 WEBHOOK_PORT = 443  # 443, 80, 88 или 8443 (порт должен быть открыт!)
 WEBHOOK_LISTEN = '0.0.0.0'  # На некоторых серверах придется указывать такой же IP, что и выше
 
@@ -14,7 +14,7 @@ WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Путь к сертификату
 WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Путь к приватному ключу
 
 WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
-WEBHOOK_URL_PATH = "/%s/" % (config.token)
+WEBHOOK_URL_PATH = "/%s/" % config.token
 
 bot = telebot.TeleBot(config.token)
 
