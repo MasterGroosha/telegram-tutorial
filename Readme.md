e11087da1f11722c4f34110d5b65c1fef6703852
Kondra007 <kondra007@yandex.ru>
2016-01-08 01:56:45 +0300

    Изменены ссылки в README.md, добавлен урок 7

--- Readme.md
+++ Readme.md
@@ -1,11 +1,13 @@
 ### Уроки по написанию простого бота для Telegram
 
-В данном репозитории хранятся исходные коды для уроков по написанию простого бота для Telegram.
+В данном репозитории хранятся исходные коды для уроков по написанию простого бота для Telegram.  
+Кстати, теперь все уроки в виде одной большой книги можно прочесть (и даже скачать) [вот тут](https://www.gitbook.com/book/kondra007/telegram-bot-lessons/details).
 
-[Урок 1: Введение, простой бот](http://goo.gl/PcN5Uh).   
-[Урок 1,5: Пояснение по библиотеке pyTelegramBotAPI](http://goo.gl/hpRIn5).  
-[Урок 2: Подготовка БД и хранилища; учимся уважать серверы Telegram](http://goo.gl/cA0xCX).  
-[Урок 3: Завершаем бота](http://goo.gl/6HV63C).  
-[Урок 4: Простой бот с использованием Webhooks](http://goo.gl/B61xbg)  
-[Урок 5: Автопостинг в каналы](http://goo.gl/D8SCXL)     
-[Урок 6: Собираем аналитику при помощи Botan](http://goo.gl/Tw3pDq)     
+[Урок 1: Введение, простой бот](https://kondra007.gitbooks.io/telegram-bot-lessons/content/chapter1.html).   
+[Урок 1,5: Работа с библиотекой pyTelegramBotAPI](https://www.gitbook.com/content/book/kondra007/telegram-bot-lessons/pytelegrambotapi_basics.html).  
+[Урок 2: Подготовка БД и хранилища; учимся уважать серверы Telegram](https://www.gitbook.com/content/book/kondra007/telegram-bot-lessons/chapter2.html).  
+[Урок 3: Завершаем бота](https://www.gitbook.com/content/book/kondra007/telegram-bot-lessons/chapter3.html).  
+[Урок 4: Простой бот с использованием Webhooks](https://www.gitbook.com/content/book/kondra007/telegram-bot-lessons/chapter4.html)  
+[Урок 5: Автопостинг в каналы](https://www.gitbook.com/content/book/kondra007/telegram-bot-lessons/chapter5.html)     
+[Урок 6: Собираем аналитику при помощи Botan](https://www.gitbook.com/content/book/kondra007/telegram-bot-lessons/chapter6.html)     
+[Урок 7: Встраиваемые боты](https://www.gitbook.com/content/book/kondra007/telegram-bot-lessons/chapter7.html)  
---
+++ lesson_07/bot.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+
+import telebot
+import re
+from telebot import types
+
+token = 'ваш_токен'
+bot = telebot.TeleBot(token)
+
+# Регулярное выражение, реагирующее на 2 введённых через пробел числа
+digits_pattern = re.compile(r'^[0-9]+ [0-9]+$', re.MULTILINE)
+
+# Иконки, выводимые в качестве превью к результатам
+plus_icon = "https://pp.vk.me/c627626/v627626512/2a627/7dlh4RRhd24.jpg"
+minus_icon = "https://pp.vk.me/c627626/v627626512/2a635/ILYe7N2n8Zo.jpg"
+divide_icon = "https://pp.vk.me/c627626/v627626512/2a620/oAvUk7Awps0.jpg"
+multiply_icon = "https://pp.vk.me/c627626/v627626512/2a62e/xqnPMigaP5c.jpg"
+error_icon = "https://pp.vk.me/c627626/v627626512/2a67a/ZvTeGq6Mf88.jpg"
+
+
+# При отсутствии запроса ( = пустой запрос) выводим некоторую информацию
+@bot.inline_handler(lambda query: len(query.query) is 0)
+def empty_query(query):
+    hint = "Введите ровно 2 числа и получите результат!"
+    try:
+        r = types.InlineQueryResultArticle(
+                id='1',
+                parse_mode='Markdown',
+                title="Бот \"Математика\"",
+                description=hint,
+                # Текст сообщения, которое будет выводиться при нажатии на подсказку
+                message_text="Эх, зря я не ввёл 2 числа :("
+        )
+        bot.answer_inline_query(query.id, [r], cache_time=86400)
+    except Exception as e:
+        print(e)
+
+
+@bot.inline_handler(lambda query: len(query.query) > 0)
+def query_text(query):
+    try:
+        matches = re.match(digits_pattern, query.query)
+    # Вылавливаем ошибку, если вдруг юзер ввёл чушь
+    # или задумался после ввода первого числа
+    except AttributeError as ex:
+        return
+    # В этом месте мы уже уверены, что всё хорошо,
+    # поэтому достаем числа
+    num1, num2 = matches.group().split()
+    try:
+        m_sum = int(num1) + int(num2)
+        r_sum = types.InlineQueryResultArticle(
+                id='1', title="Сумма",
+                # Описание отображается в подсказке,
+                # message_text - то, что будет отправлено в виде сообщения
+                description="Результат: {!s}".format(m_sum),
+                message_text="{!s} + {!s} = {!s}".format(num1, num2, m_sum),
+                # Указываем ссылку на превью и его размеры
+                thumb_url=plus_icon, thumb_width=48, thumb_height=48
+        )
+        m_sub = int(num1) - int(num2)
+        r_sub = types.InlineQueryResultArticle(
+                id='2', title="Разность",
+                description="Результат: {!s}".format(m_sub),
+                message_text="{!s} - {!s} = {!s}".format(num1, num2, m_sub),
+                thumb_url=minus_icon, thumb_width=48, thumb_height=48
+        )
+        # Учтем деление на ноль и подготовим 2 варианта развития событий
+        if num2 is not "0":
+            m_div = int(num1) / int(num2)
+            r_div = types.InlineQueryResultArticle(
+                    id='3', title="Частное",
+                    description="Результат: {0:.2f}".format(m_div),
+                    message_text="{0!s} / {1!s} = {2:.2f}".format(num1, num2, m_div),
+                    thumb_url=divide_icon, thumb_width=48, thumb_height=48
+            )
+        else:
+            r_div = types.InlineQueryResultArticle(
+                    id='3', title="Частное", description="На ноль делить нельзя!",
+                    message_text="Я нехороший человек и делю на ноль!",
+                    thumb_url=error_icon, thumb_width=48, thumb_height=48,
+                    # Сделаем превью кликабельным, по нажатию юзера направят по ссылке
+                    url="https://ru.wikipedia.org/wiki/%D0%94%D0%B5%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5_%D0%BD%D0%B0_%D0%BD%D0%BE%D0%BB%D1%8C",
+                    disable_web_page_preview=True,
+                    # Не будем показывать URL в подсказке
+                    hide_url=True
+            )
+        m_mul = int(num1) * int(num2)
+        r_mul = types.InlineQueryResultArticle(
+                id='4', title="Произведение",
+                description="Результат: {!s}".format(m_mul),
+                message_text="{!s} * {!s} = {!s}".format(num1, num2, m_mul),
+                thumb_url=multiply_icon, thumb_width=48, thumb_height=48
+        )
+        # В нашем случае, результаты вычислений не изменятся даже через долгие годы, НО!
+        # если где-то допущена ошибка и cache_time уже выставлен большим, то это уже никак не исправить (наверное)
+        # Для справки: 2147483646 секунд - это 68 с копейками лет :)
+        bot.answer_inline_query(query.id, [r_sum, r_sub, r_div, r_mul], cache_time=2147483646)
+    except Exception as e:
+        print("{!s}\n{!s}".format(type(e), str(e)))
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
