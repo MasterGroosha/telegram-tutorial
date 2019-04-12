8b487f36cfd7ce203ac95d10fb09c662b8262f41
Evgeny Petrov <groosha@protonmail.com>
2019-04-12 12:07:03 +0300

    Урок 11: обновлён код для возврата значений из Vedis

--- lesson_11/dbworker.py
+++ lesson_11/dbworker.py
@@ -8,7 +8,7 @@ import config
 def get_current_state(user_id):
     with Vedis(config.db_file) as db:
         try:
-            return db[user_id]
+            return db[user_id].decode() # Если используете Vedis версии ниже, чем 0.7.1, то .decode() НЕ НУЖЕН
         except KeyError:  # Если такого ключа почему-то не оказалось
             return config.States.S_START.value  # значение по умолчанию - начало диалога
 
