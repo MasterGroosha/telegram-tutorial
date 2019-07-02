b8c857c26a670148b0e1836e491bcdd5244da9bb
Dareten <898525@mail.ru>
2019-07-02 14:30:02 +0300

    Исправлена ошибка в обработчике пустой строки

--- lesson_07/bot.py
+++ lesson_07/bot.py
@@ -25,7 +25,6 @@ def empty_query(query):
     try:
         r = types.InlineQueryResultArticle(
                 id='1',
-                parse_mode='Markdown',
                 title="Бот \"Математика\"",
                 description=hint,
                 # Текст сообщения, которое будет выводиться при нажатии на подсказку
