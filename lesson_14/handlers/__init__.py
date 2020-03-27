5826a981fe858e578c5775dd7bbdcb0b5428d7e5
Evgeny Petrov <groosha@protonmail.com>
2020-03-27 14:55:09 +0300

    Обработка кода для напитков, разбивка хэндлеров

--- lesson_14/handlers/__init__.py
+++ lesson_14/handlers/__init__.py
@@ -1,4 +1,5 @@
 # Здесь мы подгружаем хэндлеры из файлов в текущем каталоге
+from . import general_commands
 from . import food
 from . import drinks
-from . import general_commands
+from . import default_handler
--- lesson_14/handlers/general_commands.py
+++ lesson_14/handlers/general_commands.py
@@ -9,16 +9,6 @@ from lesson_14.misc import dp
 # from misc import dp
 
 
-@dp.message_handler(commands=['start'], state="*")
-async def cmd_start(message: types.Message):
-    await message.reply("Start")
-
-
-@dp.message_handler(commands=['help'], state="*")
-async def cmd_help(message: types.Message):
-    await message.reply("Help")
-
-
 @dp.message_handler(commands="cancel", state="*")
 @dp.message_handler(Text(equals="отмена", ignore_case=True), state="*")
 async def cmd_cancel(message: types.Message, state: FSMContext):  # обратите внимание на второй аргумент
@@ -27,9 +17,7 @@ async def cmd_cancel(message: types.Message, state: FSMContext):  # обрати
     await message.answer("Действие отменено", reply_markup=types.ReplyKeyboardRemove())
 
 
-@dp.message_handler(content_types=types.ContentTypes.ANY, state="*")
-async def all_other_messages(message: types.Message):
-    if message.content_type == "text":
-        await message.reply("Ничего не понимаю!")
-    else:
-        await message.reply("Этот бот принимает только текстовые сообщения!")
+@dp.message_handler(commands=['start'], state="*")
+async def cmd_start(message: types.Message, state: FSMContext):
+    await message.reply("Выберите, что хотите заказать: "
+                        "напитки (/drinks) или блюда (/food).", reply_markup=types.ReplyKeyboardRemove())
