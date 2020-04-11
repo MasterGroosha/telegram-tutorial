7f461974648fea7cd48867828b211956f6b4d543
Evgeny Petrov <groosha@protonmail.com>
2020-04-11 20:08:44 +0300

    Редактирование списка команд

--- lesson_14/handlers/general_commands.py
+++ lesson_14/handlers/general_commands.py
@@ -3,9 +3,9 @@ from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
 
 # Если запускаете код отдельно от этого репозитория, то закомментируйте следующую строку:
-from lesson_14.misc import dp
+from lesson_14.misc import dp, bot
 # ... и замените её на:
-# from misc import dp
+# from misc import dp, bot
 
 
 @dp.message_handler(commands="cancel", state="*")
@@ -20,3 +20,12 @@ async def cmd_cancel(message: types.Message, state: FSMContext):  # обрати
 async def cmd_start(message: types.Message, state: FSMContext):
     await message.reply("Выберите, что хотите заказать: "
                         "напитки (/drinks) или блюда (/food).", reply_markup=types.ReplyKeyboardRemove())
+
+
+@dp.message_handler(commands="set_commands", state="*")
+async def cmd_set_commands(message: types.Message):
+    if message.from_user.id == 1234567:  # Подставьте сюда свой Telegram ID
+        commands = [types.BotCommand(command="/drinks", description="Заказать напитки"),
+                    types.BotCommand(command="/food", description="Заказать блюда")]
+        await bot.set_my_commands(commands)
+        await message.answer("Команды настроены.")
